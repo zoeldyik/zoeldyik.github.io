794144f8303f91eaaaf531a2a4cd4e79e5a91a74
zoeldyik <zoeldyik@gmail.com>
2020-02-09 12:11:35 +0700

    add segment web portofolio

---
+++ web-portofolio/web 1/myjs.js
@@ -0,0 +1,7 @@
+particlesJS.load('jumbo', 'particlesjs-config.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('about', 'particlesjs-config.json', function () {
+    console.log('callback - particles.js config loaded');
+});
